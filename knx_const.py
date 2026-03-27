a71262c160be3a97b8b31d323ad7a7e79f1f87f9
Nitish <nitish@gethomeassistant.com>
2026-03-27 13:01:45 +0530

    Version bump

--- knx_const.py
+++ knx_const.py
@@ -10,7 +10,7 @@ import struct
 from typing import Optional, Tuple
 
 # ── Version ───────────────────────────────────────────────────────────
-VERSION = "4.2.8"
+VERSION = "4.2.9"
 
 # ── File Paths ────────────────────────────────────────────────────────
 BACKEND_FILE        = "/run/knx-active-backend"
